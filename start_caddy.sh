816311c17ae89df0fefff620da823f7e260e966c
fchaves-cogny <fchaves@cognyhub.com>
2025-10-23 07:13:14 -0300

    fix

--- start_caddy.sh
+++ start_caddy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# copy config to tmp
 cp Caddyfile /tmp
+
+# format config
+./caddy fmt --config Caddyfile --overwrite 
+
+# export config to json file
+./caddy adapt --config Caddyfile --adapter caddyfile --pretty > /tmp/config.json
 
 # start process foreground
 ./caddy run --config "Caddyfile" --watch
