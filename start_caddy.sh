b3600cc1f02b8a8dd610d648b1649dad76a29c76
fchaves-cogny <fchaves@cognyhub.com>
2025-11-04 17:07:25 -0300

    fix

--- start_caddy.sh
+++ start_caddy.sh
@@ -2,7 +2,7 @@
 
 # format config
 ./caddy fmt --config "Caddyfile.template" --overwrite 
-cp "Caddyfile.template" Caddyfile
+cp "Caddyfile.template" "Caddyfile"
 
 # export config to json file
 #./caddy adapt --config "Caddyfile" --adapter caddyfile --pretty > /tmp/config.json
